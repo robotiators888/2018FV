fa86d104d7c5057ed1b8e0ed1c7295fba979f922
Liam Williams <liam.a.wms@gmail.com>
2018-01-24 16:22:27 -0500

    Began JSON Interpretation foundations.

---
+++ FactorOne/src/org/usfirst/frc/team888/robot/subsystems/JSONInterpreter.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team888.robot.subsystems;
+
+import java.lang.reflect.Field;
+
+import org.usfirst.frc.team888.robot.RobotMap;
+
+public class JSONInterpreter {
+
+	Field[] vars;
+	String[] varNamesJSON, varNames;
+	
+	public JSONInterpreter() {
+		vars = RobotMap.class.getDeclaredFields();
+		varNames = new String[vars.length];
+		for(int i = 0; i < vars.length; i++) varNames[i] = vars[i].getName();
+		for(int i = 0; i < varNames.length; i++) varNamesJSON[i] = camelToLower(varNames[i]);
+		
+		
+	}
+	
+	private String camelToLower(String str) {
+		String newStr = str.toLowerCase();
+		int i;
+		while((i = newStr.indexOf("_")) > -1) {
+			newStr.replace(newStr.substring(i + 1, i + 1), newStr.substring(i + 1, i + 1).toUpperCase());
+			newStr = newStr.substring(0, i - 1) + newStr.substring(i + 1);
+		}
+		return newStr;
+	}
+}
