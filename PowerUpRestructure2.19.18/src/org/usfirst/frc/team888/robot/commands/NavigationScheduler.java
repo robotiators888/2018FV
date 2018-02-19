dc59401c71d4d7f6983187af337f690cf507da55
cmtribble3 <cmtribble3@gmail.com>
2018-02-19 14:43:13 -0500

    Update for driving controls

--- PowerUpRestructure2.19.18/src/org/usfirst/frc/team888/robot/commands/NavigationScheduler.java
+++ PowerUpRestructure2.19.18/src/org/usfirst/frc/team888/robot/commands/NavigationScheduler.java
@@ -5,11 +5,21 @@ import org.usfirst.frc.team888.robot.RobotMap;
 import org.usfirst.frc.team888.robot.subsystems.Navigation;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class NavigationScheduler extends Command {
 
 	protected Navigation navigation;
 	protected OI oi;
+
+	protected double leftBaseDriveOutput = 0.0;
+	protected double rightBaseDriveOutput = 0.0;	
+	protected double leftDriveOutput = 0.0;
+	protected double rightDriveOutput = 0.0;
+	protected boolean input = false;
+	protected boolean lastInput = false;
+	protected boolean output = false;
+	protected boolean press = false;
 
 	public NavigationScheduler(Navigation p_nav, OI p_oi) {
 		requires(p_nav);
@@ -25,23 +35,45 @@ public class NavigationScheduler extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		if ((oi.getLeftStickAxis(RobotMap.L_Y_AXIS) > -0.2 
-				&& oi.getLeftStickAxis(RobotMap.L_Y_AXIS) < 0.2) 
-				&& (oi.getRightStickAxis(RobotMap.R_Y_AXIS) > -0.2 
-						&& oi.getRightStickAxis(RobotMap.R_Y_AXIS) < 0.2)) {
-			
-			navigation.navigationExecute(0.0, 0.0);
-
-		} else if (oi.getTriggers()) {
-			
-			navigation.navigationExecute(oi.getLeftStickAxis(RobotMap.L_Y_AXIS),
-					oi.getRightStickAxis(RobotMap.R_Y_AXIS));
-
+		if(oi.getTriggers()) {
+			leftBaseDriveOutput = oi.getLeftStickAxis(RobotMap.L_Y_AXIS);
+			rightBaseDriveOutput = oi.getRightStickAxis(RobotMap.R_Y_AXIS);
 		} else {
-			
-			navigation.navigationExecute(oi.getLeftStickAxis(RobotMap.L_Y_AXIS) * 0.7,
-					oi.getRightStickAxis(RobotMap.R_Y_AXIS) * 0.7);
+			leftBaseDriveOutput = 0.7 * oi.getLeftStickAxis(RobotMap.L_Y_AXIS);
+			rightBaseDriveOutput = 0.7* oi.getRightStickAxis(RobotMap.R_Y_AXIS);
 		}
+
+		if(Math.abs(oi.getLeftStickAxis(RobotMap.L_Y_AXIS)) < 0.3 &&
+				Math.abs(oi.getRightStickAxis(RobotMap.R_Y_AXIS)) < 0.3){
+			leftBaseDriveOutput = 0.0;
+			rightBaseDriveOutput = 0.0;
+		}
+
+		if (input == true && lastInput == false) {
+			press = true;
+		} else {
+			press = false;
+		}
+
+		if (press) {
+			output = !output;
+		}
+
+		lastInput = input;
+		input = oi.getLeftStickButton(2) || oi.getRightStickButton(2);
+
+		if(output) {
+			leftDriveOutput = leftBaseDriveOutput;
+			rightDriveOutput = rightBaseDriveOutput;
+		} else {
+			leftDriveOutput = -rightBaseDriveOutput;
+			rightDriveOutput = -leftBaseDriveOutput;
+		}
+
+		SmartDashboard.putNumber("leftOutput", leftDriveOutput);
+		SmartDashboard.putNumber("rightOutput", rightDriveOutput); 
+
+		navigation.navigationExecute(leftDriveOutput, rightDriveOutput);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
--- PowerUpRestructure2.19.18/src/org/usfirst/frc/team888/robot/commands/PneumaticScheduler.java
+++ PowerUpRestructure2.19.18/src/org/usfirst/frc/team888/robot/commands/PneumaticScheduler.java
@@ -37,6 +37,7 @@ public class PneumaticScheduler extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
+		
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
