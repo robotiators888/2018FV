199fd50b48fd073555845e8046086b56d49da31b
888robotiators <webmaster@robotiators888.org>
2018-02-20 17:11:58 -0500

    working on multi camera

--- showDS.py
+++ showDS.py
@@ -15,6 +15,11 @@ import struct
 import sys
 import time
 
+import numpy as np
+import Tkinter as tk
+from PIL import Image, ImageTk
+import threading
+
 
 # define frame fragment maximum send size
 maxFragmentSize = 61440
@@ -29,20 +34,57 @@ cameraHost    = "10.8.88.12"
 cameraPort    = 8888
 cameraAddress = (cameraHost, cameraPort)
 
+
 # create a UDP socket endpoint
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # attaches socket to server address
 sock.bind(ADDRESS)
 
-
 inputs = [sock]
 outputs = []
 
+fragmentList = []
+
 state = 'idle'
 
-# keep looping
-while True:
+cameraMessage = "frontCamera"
+
+sock.sendto(cameraMessage, cameraAddress)
+
+# set up GUI
+root = tk.Tk() #makes main window
+root.wm_title("camera feed")
+root.config(background="#FFFFFF")
+
+# graphics window
+imageFrame = tk.Frame(root, width=600, height=500)
+imageFrame.grid(row=0, column=0, padx=10, pady=2)
+
+lmain = tk.Label(imageFrame)
+lmain.grid(row=0, column=0)
+
+def switchToBack():
+    print "back"
+    cameraMessage = "backCamera"
+    sock.sendto(cameraMessage, cameraAddress)
+
+def switchToFront():
+    print "front"
+    cameraMessage = "frontCamera"
+    sock.sendto(cameraMessage, cameraAddress)
+
+b = tk.Button(root, text="FRONT", command=switchToFront)
+b.grid(row=1, column=0)
+
+b = tk.Button(root, text="BACK", command=switchToBack)
+b.grid(row=1, column=1)
+
+def show():
+  state = 'idle'
+  
+  # keep looping
+  while True:
 
     readable, _, _ = select.select(inputs, outputs, inputs, 0.25)
 	
@@ -50,10 +92,10 @@ while True:
     try:
 	    if readable:
 		    # attempt to read message from socket
-			#msg, address = sock.recvfrom(65507)
+		    #msg, address = sock.recvfrom(65507)
 		    msg, address = sock.recvfrom(65536)
 	    else: 
-		    sock.sendto("frontCamera", cameraAddress)
+		    sock.sendto(cameraMessage, cameraAddress)
 		    msg = None
 		    address = None
 
@@ -67,24 +109,28 @@ while True:
     # if a message was received...
     if msg and (len(msg) >= 8):
 
-        # process current message
+        # process current message.
 
         msgId, msgLength = struct.unpack('!2I', msg[0:8])
 
+        
         # If this is message 10....
         if msgId == 10:
 
             # process message 10
-
+            
             fragmentSize = msgLength - 8
 
-            flattenedFrame, = \
-                struct.unpack("!8x{0}s".format(fragmentSize), msg)
+            fragment, = struct.unpack("!8x{0}s".format(fragmentSize), msg)
 
+            #flattenedFrame = fragment
+            fragmentList = [fragment]
+            
             state = 'loading'
+        
 
         # If this is message 11....
-        elif msgId == 11:
+        if msgId == 11:
 
             if state == 'loading':
 
@@ -97,21 +143,38 @@ while True:
                 fragment = \
                     struct.unpack("!8x2I{0}s".format(fragmentSize), msg)
 
-                flattenedFrame += fragment
+                #flattenedFrame += fragment
+
+                fragmentList.append(fragment)
 
                 if last == 1:
                     state = 'show'
 
     if state == 'show':
-
+        
         try:
+            flattenedFrame = ''.join(fragmentList)
+            
             imgencode = pickle.loads(flattenedFrame)
             frame = cv2.imdecode(imgencode, 1)
-            cv2.imshow("Frame", frame)
-            cv2.waitKey(100)
+
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            img = Image.fromarray(cv2image)
+            imgtk = ImageTk.PhotoImage(image=img)
+
+            lmain.imgtk = imgtk
+            lmain.configure(image=imgtk)
+            
+            #cv2.imshow("Frame", frame)
+            #cv2.waitKey(100)
         except Exception:
             None
+            
         state = 'idle'
 
-# from fps_testing
-vs.stop()
+
+threading.Thread(target=show).start()
+
+root.mainloop()
+
+
