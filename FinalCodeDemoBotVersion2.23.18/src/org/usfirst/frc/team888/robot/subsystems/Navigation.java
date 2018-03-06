7f128bd04ec4a401f25a113cece05f783f145279
888robotiators <webmaster@robotiators888.org>
2018-03-06 16:55:49 -0500

    line spacing fixed

--- FinalCodeDemoBotVersion2.23.18/src/org/usfirst/frc/team888/robot/subsystems/Navigation.java
+++ FinalCodeDemoBotVersion2.23.18/src/org/usfirst/frc/team888/robot/subsystems/Navigation.java
@@ -374,8 +374,6 @@ public class Navigation extends Subsystem {
 
 	}
 
-
-
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		//setDefaultCommand(new MySpecialCommand());
